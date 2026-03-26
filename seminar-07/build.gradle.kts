85207cbfb7fd426144694c96fa2b2c234bb79c2b
Stanislau Palyn <stanislau.palyn@jetbrains.com>
2026-03-26 18:13:28 +0100

    Add build gradle

---
+++ seminar-07/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "org.edu.jvm.languages"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    testImplementation(kotlin("test-junit"))
+    testImplementation("org.jetbrains.kotlinx:lincheck:2.28.1")
+}
+
+repositories {
+    mavenCentral()
+}
+
+sourceSets.main {
+    java.srcDir("src")
+}
+
+sourceSets.test {
+    java.srcDir("test")
+}
+
+tasks {
+    test {
+        maxHeapSize = "6g"
+        jvmArgs(
+            "--add-opens", "java.base/jdk.internal.misc=ALL-UNNAMED",
+            "--add-exports", "java.base/jdk.internal.util=ALL-UNNAMED",
+            "--add-exports", "java.base/sun.security.action=ALL-UNNAMED"
+        )
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
